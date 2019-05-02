673f2ef897fd0c399d9e484b980c88f7ed2b7747
Tommi <tommi.tarkki@gmail.com>
2019-05-02 18:28:17 +0300

    changes app into arrow func

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import View from './components/common/view';
 
-function App() {
-  return (
-    <div className="App" style={{ display: 'flex' }}>
-      <View dataToView="emissions" />
-      <View dataToView="population" />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App" style={{ display: 'flex' }}>
+    <View dataToView="emissions" />
+    <View dataToView="population" />
+  </div>
+);
 
 export default App;
